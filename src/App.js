4883447b1963e9d6afa134af30532ea9fc13d89b
Ifiok Udoidiok <ifioku@wolfram.com>
2021-09-27 15:35:16 +0100

    Import browser router and implement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import { GlobalProvider } from './context/GlobalState';
+
+import { Home } from './components/Home';
+import { AddEvent } from './components/AddEvent';
+import { EditEvent } from './components/EditEvent';
+
+
+// import "./styles.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GlobalProvider>
+    <div >
+      <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={AddEvent} exact />
+          <Route path="/edit/:id" component={EditEvent} exact />
+        </Switch>
     </div>
+    </GlobalProvider>
   );
 }
-
-export default App;
